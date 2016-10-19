6ef52b065de33a18059afaa482c3753b41718bf8
Jenn Kao <jennkao92@gmail.com>
2016-10-19 12:36:46 -0700

    Add get request handler with Express for path "/" to index.html

--- server.js
+++ server.js
@@ -3,7 +3,15 @@
 const express = require('express');
 const app = express();
 
-//created the server with express
+//created the server with express listening at port 3000
 app.listen(3000, function() {
 	console.log('listening on 3000');
+});
+
+// app.get(pathname, callback) 
+//when user wants to get this pathname
+//__dirname is the path to current working directory
+//sendFile: Express serves an index.html file found in root folder
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
 });
