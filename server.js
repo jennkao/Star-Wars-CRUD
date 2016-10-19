37b145c5b1d150d43128561b8bf05e06f6491545
Jenn Kao <jennkao92@gmail.com>
2016-10-19 14:31:23 -0700

    Add mongoDB database from MongoLabs to application

--- server.js
+++ server.js
@@ -6,15 +6,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+var db;
 
 //urlencoded method tells body-parser to extract data from the form
 //and add that data to request.body property
 app.use(bodyParser.urlencoded({extended: true}));
-
-//created the server with express listening at port 3000
-app.listen(3000, function() {
-	console.log('listening on 3000');
-});
 
 //CRUD-READ: app.get(pathname, callback)
 //when user wants to get this pathname
@@ -29,4 +26,26 @@ app.get('/', (req, res) => {
 //Express app. Method tells browser what request to send.
 app.post('/quotes', (req, res) => {
 	console.log(req.body);
-})
+});
+
+
+var linkToMongo='mongodb://<jennkao>:<92mongo>@ds021751.mlab.com:' + 
+'21751/star-wars-quotes'; //link to our Mongo db hosted on mongo cloud
+MongoClient.connect(linkToMongo, (err, database) => { 
+	if (err) {
+		return console.log(err);
+	}
+	db = database; //database parameter contains the initialized db object or 
+	//null if an error connecting to db occurs;
+
+	//create the server with express listening at port 3000
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	});
+
+});
+
+
+
+
+
