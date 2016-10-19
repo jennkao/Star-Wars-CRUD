3e37784b622dc10369b9a82719cb23f4cd60c54f
Jenn Kao <jennkao92@gmail.com>
2016-10-19 12:25:19 -0700

    Create a server using express

---
+++ server.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+//created the server with express
+app.listen(3000, function() {
+	console.log('listening on 3000');
+});
